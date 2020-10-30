8dbe7c78a7cb6233cb5f3cf9b9cfc8c5e6f2c4f0
aleksandarm03 <accam003@gmail.com>
2020-10-30 14:39:18 +0100

    update: 30.10.2020.

---
+++ Balon/Balon/Balon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+namespace Balon
+{
+    class Balon
+    {
+        Random r=new Random();
+        int x, y;
+        public Balon(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Crtaj(Graphics g)
+        {
+            int a = 15;
+            SolidBrush cetkica = new SolidBrush(Color.FromArgb(r.Next(255), r.Next(255), r.Next(255)));
+            Pen olovka = new Pen(Color.Black, 1);
+            PointF[] points ={new PointF(x,y-3),new PointF(x-a,y+a), new PointF(x+a,y+a)};
+            g.FillEllipse(cetkica,x-3*a,y-8*a,6*a,8*a);
+            g.FillPolygon(cetkica, points);
+            
+            g.DrawLine(olovka,x,y+a,x-a,y+2*a);
+            g.DrawLine(olovka, x - a, y + 2 * a, x + a, y + 3 * a);
+            g.DrawLine(olovka, x + a, y + 3 * a, x - a, y + 4 * a);
+            g.DrawLine(olovka, x - a, y + 4 * a,x+a,y+5*a);
+        }
+       
+    }
+}
---
+++ Balon/Balon/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Balon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        
+        
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            int x = e.X;
+            int y = e.Y;
+            Balon b = new Balon(x,y);
+            b.Crtaj(g);
+            
+        }
+
+        
+            
+        
+    }
+}
